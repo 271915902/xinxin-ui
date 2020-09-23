eeba9f535db0836659ab642d0a3a6543bfda2441
阿鑫 <8101943+qqxinaaaaa@user.noreply.gitee.com>
2020-09-24 04:19:31 +0800

    地址

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
       <h1>阿鑫UI</h1>
       <h2>一个简单的 UI 框架</h2>
       <p class="actions">
-        <a href="https://github.com">GitHub</a>
+        <a href="https://github.com/271915902/xinxin-ui">GitHub</a>
         <router-link to="/doc">开始</router-link>
       </p>
     </div>
