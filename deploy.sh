321a4a3b53f0c6d76dc313004b11747d758ac6de
阿鑫 <8101943+qqxinaaaaa@user.noreply.gitee.com>
2020-09-25 10:41:20 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ git init
 git add .
 git commit -m "更新"
 git branch -M master
-git remote add origin git@gitee.com:qqxinaaaaa/xinxinui-website.git
+git remote add origin git@github.com:271915902/xinxinui-website.git
 git push -f -u origin master 
 cd -
